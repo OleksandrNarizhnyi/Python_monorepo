539bd3c871440320ff52c7d601396f0bd96d937b
OleksandrNarizhnyi <ssani4@icloud.com>
2025-02-17 18:56:00 +0100

    Delete Python_lectures_practik/practicum_20/dbconfig.py

--- Python_lectures_practik/practicum_20/dbconfig.py
+++
@@ -1,9 +0,0 @@
-from pymysql.cursors import DictCursor
-dbconfig = {
-    'host': 'ich-edit.edu.itcareerhub.de',
-    'user': 'ich1',
-    'password': 'ich1_password_ilovedbs',
-    'database': 'Sakila',
-    'charset': 'utf8mb4',
-    'cursorclass': DictCursor
-    }
